17f308bb2f634a5ce3d1496ed93af0925f24e115
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-07-08 23:31:05 +0800

    max_col increase

--- pyunidoe/doe_optimizer.cpp
+++ pyunidoe/doe_optimizer.cpp
@@ -73,7 +73,7 @@ Optimizer::Optimizer(vector<vector<double> > x_init, int nnew_init, int np_init,
     allpairs = nnew * (nnew -1) / 2;
     for (i=0;i<nv;i++)
     {
-        maxcol += 20 * optimize_columns[i];
+        maxcol += 50 * optimize_columns[i];
         allpairs_temp = allpairs;
         max_freq = 0; min_freq = nnew;
         valid_nlevel = 0, valid_nvalues = 0;
@@ -109,7 +109,7 @@ Optimizer::Optimizer(vector<vector<double> > x_init, int nnew_init, int np_init,
         if (maxpairs<nlevel_pairs[i]) maxpairs=nlevel_pairs[i];
         if (maxpairs<nelement_pairs[i]) maxpairs=nelement_pairs[i];
     }
-    maxcol = min(max(maxcol, 10), 100);
+    maxcol = min(max(maxcol, 50), 200);
     //  Initialize the exchange index list
     ind1.assign(maxpairs, 0);
     ind2.assign(maxpairs, 0);
